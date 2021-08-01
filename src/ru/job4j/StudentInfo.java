d9de86c7b3ea814978563c820ca9a84056872d5a
MaxxximBut <maxxximbute@mail.ru>
2021-08-01 23:40:29 +0300

    3.3. Разбор первой программы. [#159033]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Maxim Butenko");
+        System.out.println("31.01.1997");
+    }
+}
