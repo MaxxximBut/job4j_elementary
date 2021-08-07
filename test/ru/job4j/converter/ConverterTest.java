ba2d45e18a79deeab91ea300e5b0bd1ca859bc98
MaxxximBut <maxxximbute@mail.ru>
2021-08-07 17:29:07 +0300

    1. Модульные тесты. [#159435 #186528]

--- test/ru/job4j/converter/ConverterTest.java
+++ test/ru/job4j/converter/ConverterTest.java
@@ -17,14 +17,14 @@ public class ConverterTest {
     @Test
     public void whenConvert180RblThen3Dlr() {
         float in = 180;
-        float expected = 2;
+        float expected = 3;
         float out = Converter.rubleToDollar(in);
         float eps = 0.0001f;
         Assert.assertEquals(expected, out, eps);
     }
 
     @Test
-    public void whenConvert0RblThen2Euro() {
+    public void whenConvert0RblThen0Euro() {
         float in = 0f;
         float expected = 0f;
         float out = Converter.rubleToEuro(in);
@@ -33,9 +33,9 @@ public class ConverterTest {
     }
 
     @Test
-    public void whenConvert0RblThen3Dlr() {
+    public void whenConvert0RblThen0Dlr() {
         float in = 0f;
-        float expected = 3;
+        float expected = 0;
         float out = Converter.rubleToDollar(in);
         float eps = 0.0001f;
         Assert.assertEquals(expected, out, eps);
