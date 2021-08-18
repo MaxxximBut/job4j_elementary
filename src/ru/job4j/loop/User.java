cf4fed16c29bc88e4d4b0fbfda6d1a9b304256e7
MaxxximBut <maxxximbute@mail.ru>
2021-08-19 00:14:30 +0300

    5.8. Именование классов и переменных [#4087]

---
+++ src/ru/job4j/loop/User.java
@@ -0,0 +1,12 @@
+package ru.job4j.loop;
+
+public class User {
+            public static void convert() {
+            String name = "Maxim Butenko";
+            boolean isYourName = true;
+            int age = 24;
+        }
+
+            public static void main(String[] args) {
+        }
+    }
